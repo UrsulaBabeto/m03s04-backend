5a1e5ed675c6f52966e929dc79c10059334cbf4d
Ursula Babeto <ursulacobabeto@gmail.com>
2023-09-03 20:12:14 -0300

    update users auth middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,24 @@
-const { create, findAll, findOne, login, findCarts } = require('../controller/user.controller')
-const { Router } = require('express')
-/* const { auth } = require('../middlewares/auth.middleware')
- */
+const {
+  create,
+  findAll,
+  findOne,
+  login,
+  findCarts,
+} = require("../controller/user.controller");
+const { Router } = require("express");
+const { auth } = require("../middlewares/auth.middleware");
 
-class UserRouter{
-    routesFromUser() {
-        const userRoutes = Router()
-        userRoutes.post('/users', create)
-        userRoutes.get('/users', findAll)
-        userRoutes.get('/users/:userId', findOne)
-       /*  userRoutes.post('/users/login', login) */
-        userRoutes.get('/users/:userId/carts', findCarts)
+class UserRouter {
+  routesFromUser() {
+    const userRoutes = Router();
+    userRoutes.post("/users", create);
+    userRoutes.get("/users", auth, findAll);
+    userRoutes.get("/users/:userId", auth, findOne);
+    userRoutes.post("/users/login", auth, login);
+    userRoutes.get("/users/:userId/carts", auth, findCarts);
 
-        return userRoutes
-    }
+    return userRoutes;
+  }
 }
 
-module.exports = new UserRouter()
+module.exports = new UserRouter();
